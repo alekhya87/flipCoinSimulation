e4c28639958bfe434da1f63aaf8729a6dd79d8b2
alekhya87 <davulurialekhya87909.com>
2020-10-06 20:55:28 +0530

    UseCase-1 is done

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,2 +1,11 @@
 #!/bin/bash/ -x
 echo "flip Coin Simulation"
+#useCase-1
+isHead=1
+randomCheck=$((RANDOM%2))
+if [ $isHead -eq $randomCheck ]
+then
+	echo "Head is Winner"
+else
+	echo "Tail is Winner"
+fi
